c7ef73f935df0f950c3103cfd59ca97d7e81a4c0
André Luiz Alves <39349276+Oevidente@users.noreply.github.com>
2026-06-11 13:29:05 -0300

    build: configure GitHub Pages deployment
    Update Vite base path for relative asset loading and add a workflow to automate deployment to GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite';
 
 export default defineConfig({
+  base: './',
   server: {
     // Disable HMR because the application uses vanilla JS is updated in turns
     hmr: false,
